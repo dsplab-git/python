5e1a66d3142eecef560a3f1a9fca10379dce5013
dongsung.pae <paeds915@gmail.com>
2025-04-07 11:02:06 +0900

    4주차 강의 실습 자료 마지막

--- CanvasTest.py
+++ CanvasTest.py
@@ -1,10 +1,37 @@
 import tkinter
+import random
+from PIL import ImageTk, Image
+
 root = tkinter.Tk()
 root.title("캔버스 만들기")
-canvas = tkinter.Canvas(root, width=800, height=600, bg="skyblue")
 
-bgimg = tkinter.PhotoImage(file="miko.png")
+#좌표 출력기
+def mouseMove(event):
+    x = event.x
+    y = event.y    
+    labelMouse["text"]=str(x)+","+str(y)
+
+def click_btn():
+    label["text"] = random.choice(["대길","중길","소길","흉"])
+
+# 캔버스 생성
+canvas = tkinter.Canvas(root, width=800, height=600, bg="skyblue")
+canvas.pack()
+
+#좌표 출력기
+root.bind("<Motion>", mouseMove)
+labelMouse = tkinter.Label(root, text=",", font=("맑은 고딕",10))
+labelMouse.pack()
+
+# 캔버스 내 이미지 생성
+#bgimg = tkinter.PhotoImage(file="miko.png")
+bgimg = ImageTk.PhotoImage(Image.open('miko.png'))
 canvas.create_image(400,300,image=bgimg)
 
-canvas.pack()
+label = tkinter.Label(root, text="??", font=("맑은 고딕",120))
+label.place(x=380, y=60)
+
+button = tkinter.Button(root, text="제비뽑기", font=("맑은 고딕",36),command=click_btn)
+button.place(x=360, y=400)
+
 root.mainloop()
